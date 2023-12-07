637b4f88452ef9ee298212dd88d904389f5ed951
@ayoubhofr <ayoubhofr@gmail.com>
2023-12-07 15:42:10 +0100

    config v2

---
+++ customer-service/src/main/java/org/sid/customerservice/web/CustomerConfigTestController.java
@@ -0,0 +1,27 @@
+package org.sid.customerservice.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class CustomerConfigTestController {
+    @Value("${global.params.p1}")
+    public String p1;
+    @Value("${global.params.p2}")
+
+    public String p2;
+    @Value("${global.params.x}")
+
+    public String x;
+    @Value("${global.params.y}")
+
+    public String y;
+
+    @GetMapping("/params")
+    public Map<String,String> params(){
+        return Map.of("p1",p1,"p2",p2,"x",x,"y",y);
+    }
+}
