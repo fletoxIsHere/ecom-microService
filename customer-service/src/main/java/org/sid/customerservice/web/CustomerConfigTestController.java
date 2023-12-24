a123def569f12670f59b60728e1b64f9c10cbf4e
@ayoubhofr <ayoubhofr@gmail.com>
2023-12-24 20:09:51 +0100

    v3

--- customer-service/src/main/java/org/sid/customerservice/web/CustomerConfigTestController.java
+++ customer-service/src/main/java/org/sid/customerservice/web/CustomerConfigTestController.java
@@ -1,11 +1,12 @@
 package org.sid.customerservice.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+//@RefreshScope
 @RestController
 public class CustomerConfigTestController {
     @Value("${global.params.p1}")
@@ -13,15 +14,15 @@ public class CustomerConfigTestController {
     @Value("${global.params.p2}")
 
     public String p2;
-    @Value("${global.params.x}")
-
-    public String x;
-    @Value("${global.params.y}")
-
-    public String y;
+//    @Value("${customer.params.x}")
+//
+//    public String x;
+//    @Value("${customer.params.y}")
+//
+//    public String y;
 
     @GetMapping("/params")
     public Map<String,String> params(){
-        return Map.of("p1",p1,"p2",p2,"x",x,"y",y);
+        return Map.of("p1",p1,"p2",p2);
     }
 }
