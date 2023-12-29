df7d960d4492415988fb51243a1c4d2ef3b752a2
@ayoubhofr <ayoubhofr@gmail.com>
2023-12-29 21:24:45 +0100

    update removing eureka

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -5,12 +5,14 @@ import org.sid.inventoryservice.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
