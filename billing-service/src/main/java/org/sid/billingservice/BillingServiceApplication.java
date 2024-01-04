baea77caf01b632f2c5d81fd3d6dd02ab8dab06a
@ayoubhofr <ayoubhofr@gmail.com>
2024-01-05 00:40:41 +0100

    fix consul pom file

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -11,8 +11,6 @@ import org.sid.billingservice.services.ProductRestClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
